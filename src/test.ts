34715bd105b27cac99e3e7c16d9b14475dbce5d1
Vedansh Shetti <vedansh.shetti@gmail.com>
2026-04-03 00:12:32 +0200

    fixed issues

--- src/test.ts
+++ src/test.ts
@@ -1,8 +1,20 @@
 import { httpsets } from "./main.ts";
 
-console.log("Testing GET request...");
-console.time("Total time");
-const res = await httpsets.GET<{ userId: number, id: number, title: string, completed: boolean }>("https://jsonplaceholder.typicode.com/todos/1");
-console.timeEnd("Total time");
+async function test() {
+    console.log("Testing GET request...");
+    console.time("GET");
 
-console.log(JSON.stringify(res, null, 2));
+    const res = await httpsets.GET<{
+      userId: number;
+      id: number;
+      title: string;
+      completed: boolean;
+    }[]>("https://jsonplaceholder.typicode.com/todos/");
+
+    const data = res.json;
+
+    console.timeEnd("GET");
+    console.log("GET result (first 2 items):", data.slice(0, 2)); // print first 2 items
+}
+
+test();
